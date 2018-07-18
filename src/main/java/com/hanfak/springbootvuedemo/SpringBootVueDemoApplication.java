b0d24c350907f73a81b043634d2a9c4f4412f101
Hanif Fakira <fakira.work@gmail.com>
2018-07-18 14:07:37 +0100

    initial commit

---
+++ src/main/java/com/hanfak/springbootvuedemo/SpringBootVueDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hanfak.springbootvuedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVueDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVueDemoApplication.class, args);
+	}
+}
